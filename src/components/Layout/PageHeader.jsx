63098f9c0f3385a86abd35f2788d99280e2db3aa
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-11-18 03:17:55 +0300

    Update content & style of PageHeader

--- src/components/Layout/PageHeader.jsx
+++ src/components/Layout/PageHeader.jsx
@@ -1,12 +1,22 @@
 import Logo from './Logo.jsx';
+import SiteList from '../UI/SiteList.jsx';
+import ShowCartButton from '../Cart/ShowCartButton.jsx';
+import Intro from './Intro/Intro.jsx';
 import classes from './PageHeader.module.scss';
 
 function PageHeader() {
   return (
     <header className={classes['page-header']}>
-      <div className={classes['page-header__wrapper']}>
-        <Logo />
+      <div className={`${classes['page-header__wrapper']} wrapper`}>
+        <Logo className={classes['page-header__logo']} />
+        <nav className={classes['page-header__nav']} aria-label="Primary">
+          <SiteList />
+        </nav>
+        <div className={classes['page-header__user-tools']}>
+          <ShowCartButton amount="2" />
+        </div>
       </div>
+      <Intro />
     </header>
   );
 }
