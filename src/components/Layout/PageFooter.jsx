13c35777182646eb32903b4e2ec1cd07b9761530
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-11-18 03:18:58 +0300

    Update content & style of PageFooter component

--- src/components/Layout/PageFooter.jsx
+++ src/components/Layout/PageFooter.jsx
@@ -1,11 +1,13 @@
 import Logo from './Logo.jsx';
+import Socials from '../UI/Socials.jsx';
 import classes from './PageFooter.module.scss';
 
 function PageFooter() {
   return (
     <footer className={classes['page-footer']}>
-      <div className={classes['page-footer__wrapper']}>
+      <div className={`${classes['page-footer__wrapper']} wrapper`}>
         <Logo />
+        <Socials />
       </div>
     </footer>
   );
