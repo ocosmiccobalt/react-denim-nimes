e7f2c5baae10dd6f5ef26a26f354e92e071b78f7
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-11-18 03:10:35 +0300

    Add Hero (based on reusable Picture) & assets for it

---
+++ src/components/Layout/Intro/Hero.jsx
@@ -0,0 +1,29 @@
+import Picture from '../../UI/Picture.jsx';
+
+function Hero({ className = '', imgClassName = '' }) {
+  const size = {
+    mWidth: 202,
+    mHeight: 249,
+    tWidth: 264,
+    tHeight: 319,
+    dWidth: 430,
+    dHeight: 517
+  };
+
+  return (
+    <p
+      className={className}
+      style={{ '--tablet-aspect-ratio': '24 / 29', '--desktop-aspect-ratio': '430 / 517' }}
+    >
+      <Picture
+        srcName='hero'
+        size={size}
+        alt="Confident girl wearing denim jacket."
+        imgClassName={imgClassName}
+        webWitals={{ fetchpriority: 'high' }}
+      />
+    </p>
+  );
+}
+
+export default Hero;
