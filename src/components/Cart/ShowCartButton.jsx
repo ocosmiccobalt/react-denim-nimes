d560e64ae78618754c0eec7fee1cb1027f5b0f95
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-11-29 23:49:55 +0300

    Provide better formatting for ShowCartButton.jsx

--- src/components/Cart/ShowCartButton.jsx
+++ src/components/Cart/ShowCartButton.jsx
@@ -2,13 +2,23 @@ import classes from './ShowCartButton.module.scss';
 
 function ShowCartButton({ amount, onClick }) {
   return (
-    <button className={classes['show-cart-button']} type="button" onClick={onClick}>
-      <span className="visually-hidden">Open the cart</span>
+    <button
+      className={classes['show-cart-button']}
+      type="button"
+      onClick={onClick}
+      aria-label="Open the cart"
+    >
       <span className={classes['show-cart-button__amount']}>
         <span className="visually-hidden">Amount of items:</span>
         <span>{amount}</span>
       </span>
-      <svg className={classes['show-cart-button__icon']} width="20" height="18" aria-hidden="true" focusable="false">
+      <svg
+        className={classes['show-cart-button__icon']}
+        width="20"
+        height="18"
+        aria-hidden="true"
+        focusable="false"
+      >
         <use xlinkHref="#icon-cart"></use>
       </svg>
     </button>
