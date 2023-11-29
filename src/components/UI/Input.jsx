ff449a65f28fd1dfbdd30880550ecfcde10b382d
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-11-30 00:04:53 +0300

    Add the Input component (for the most of input types)

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,23 @@
+import { forwardRef } from 'react';
+import classes from './Input.module.scss';
+
+const Input = forwardRef(function Input(
+  {
+    Container = 'li',
+    className = '',
+    label,
+    input = {}
+  },
+  ref
+) {
+  return (
+    <Container className={`${classes.input} ${className}`}>
+      <label className={classes.input__label} htmlFor={input.id}>
+        {label}
+      </label>
+      <input className={classes.input__field} ref={ref} {...input} />
+    </Container>
+  );
+});
+
+export default Input;
