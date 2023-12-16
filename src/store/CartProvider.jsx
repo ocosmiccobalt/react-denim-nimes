906aade43a236bc18f7a104363f92191e9812b4b
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-12-16 19:48:36 +0300

    Add a cart reducer

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -1,13 +1,39 @@
+import { useReducer } from 'react';
+
 import CartContext from './cart-context.jsx';
 
-function CartProvider({ children }) {
-  function handleAddItemToCart() {}
+const defaultCartState = {
+  items: [],
+  total: 0
+};
 
-  function handleRemoveItemFromCart() {}
+function cartReducer(state, action) {
+  if (action.type === 'ADD') {
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotal = state.total + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      total: updatedTotal
+    };
+  }
+
+  return defaultCartState;
+}
+
+function CartProvider({ children }) {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+  function handleAddItemToCart(item) {
+    dispatchCartAction({ type: 'ADD', item: item });
+  }
+
+  function handleRemoveItemFromCart(id) {
+    dispatchCartAction({ type: 'REMOVE', id: id });
+  }
 
   const cartContext = {
-    items: [],
-    total: 0,
+    items: cartState.items,
+    total: cartState.total,
     addItem: handleAddItemToCart,
     removeItem: handleRemoveItemFromCart
   };
