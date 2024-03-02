aacbe74985e299b87219961300a45212a624b8a2
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-03-02 15:55:28 +0300

    Add initial data to the backend

---
+++ backend/util/writeDummyProducts.js
@@ -0,0 +1,9 @@
+import fs from 'node:fs/promises';
+
+import DUMMY_PRODUCTS from '../data/dummyProducts.js';
+
+async function writeJSON() {
+  await fs.writeFile('./data/available-products.json', JSON.stringify(DUMMY_PRODUCTS, null, 2));
+}
+
+await writeJSON();
