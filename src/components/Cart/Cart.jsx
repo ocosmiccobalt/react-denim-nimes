a35a16db912de996046823fd046ab3c4aea6b42d
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-03-07 00:07:19 +0300

    Recreate Cart cpm using Modal & user progress context

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,35 +1,25 @@
-import {
-  forwardRef,
-  useRef,
-  useImperativeHandle,
-  useContext
-} from 'react';
-import { createPortal } from 'react-dom';
+import { useContext } from 'react';
 
-import Button from '../UI/Button.jsx';
 import CartContext from '../../store/cart-context.jsx';
+import UserProgressContext from '../../store/user-progress-context.jsx';
+import Modal from '../UI/Modal.jsx';
 import CartItem from './CartItem.jsx';
+import Button from '../UI/Button.jsx';
 import classes from './Cart.module.scss';
 
-const Cart = forwardRef(function Cart({ onClose }, ref) {
+function Cart() {
   const cartCtx = useContext(CartContext);
-  const dialog = useRef();
-
-  useImperativeHandle(ref, () => {
-    return {
-      open() {
-        dialog.current.showModal();
-      }
-    };
-  });
-
-  const portalElement = document.getElementById('overlays');
+  const userProgressCtx = useContext(UserProgressContext);
 
   const total = `$ ${cartCtx.total.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
-  function handleCloseCartClick() {
-    dialog.current.close();
+  function handleCloseCart() {
+    userProgressCtx.hideCart();
+  }
+
+  function handleGoToCheckout() {
+    userProgressCtx.showCheckout();
   }
 
   function handleCartItemRemove(id) {
@@ -53,32 +43,32 @@ const Cart = forwardRef(function Cart({ onClose }, ref) {
     />
   ));
 
-  return createPortal(
-    <dialog className={classes.cart} ref={dialog} onClose={onClose}>
-      <div className={classes.cart__content}>
-        <h2 className={classes.cart__title}>Your cart</h2>
-        <ul className={classes.cart__list} role="list">
-          {cartItems}
-        </ul>
-        <p className={classes.cart__summary}>
-          <span>Total:{' '}</span>
-          <span>{total}</span>
-        </p>
-        <div>
-          <button
-            className={classes.cart__close}
-            onClick={handleCloseCartClick}
-            type="button"
-            aria-label="Close the cart"
-          >
-            &times;
-          </button>
-          {hasItems && <Button>Order</Button>}
-        </div>
+  return (
+    <Modal
+      open={userProgressCtx.progress === 'cart'}
+      onClose={userProgressCtx.progress === 'cart' ? handleCloseCart : null}
+    >
+      <h2 className={classes.cart__title}>Your cart</h2>
+      <ul className={classes.cart__list} role="list">
+        {cartItems}
+      </ul>
+      <p className={classes.cart__summary}>
+        <span>Total:{' '}</span>
+        <span>{total}</span>
+      </p>
+      <div>
+        <button
+          className={classes.cart__close}
+          onClick={handleCloseCart}
+          type="button"
+          aria-label="Close the cart"
+        >
+          &times;
+        </button>
+        {hasItems && <Button onClick={handleGoToCheckout}>Order</Button>}
       </div>
-    </dialog>,
-    portalElement
+    </Modal>
   );
-});
+}
 
 export default Cart;
