b0cdba554a5e03414dfbf06f78af8b244e5e8523
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-03-06 23:58:53 +0300

    Use a new ctx layer (user progress) in the App cmp

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,23 @@
-import { useState, useRef } from 'react';
-
 import PageHeader from './components/Layout/PageHeader.jsx';
 import PageMain from './components/Layout/PageMain.jsx';
 import Products from './components/Products/Products.jsx';
 import PageFooter from './components/Layout/PageFooter.jsx';
 import Cart from './components/Cart/Cart.jsx';
 import CartProvider from './store/CartProvider.jsx';
+import UserProgressProvider from './store/UserProgressProvider.jsx';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-  const cartDialog = useRef();
-
-  if (cartIsShown) {
-    cartDialog.current.open();
-  }
-
-  function handleShowCart() {
-    setCartIsShown(true);
-  }
-
-  function handleCloseCart() {
-    setCartIsShown(false);
-  }
-
   return (
-    <CartProvider>
-      <Cart ref={cartDialog} onClose={handleCloseCart} />
-      <PageHeader onShowCart={handleShowCart} />
-      <PageMain>
-        <Products sectionId='products' />
-      </PageMain>
-      <PageFooter />
-    </CartProvider>
+    <UserProgressProvider>
+      <CartProvider>
+        <Cart />
+        <PageHeader />
+        <PageMain>
+          <Products sectionId='products' />
+        </PageMain>
+        <PageFooter />
+      </CartProvider>
+    </UserProgressProvider>
   );
 }
 
