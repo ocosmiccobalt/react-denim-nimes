e83a2907c596bb842ba0e81332d1db102bf64a11
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-03-07 18:24:21 +0300

    Manage error & loading state in the Products cmp

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,21 +5,39 @@ import classes from './Products.module.scss';
 
 function Products({ sectionId }) {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     async function fetchProducts() {
       const response = await fetch('http://localhost:3000/products');
 
       if (!response.ok) {
-        // ...
+        throw new Error('Something went wrong, failed to fetch products.');
       }
 
       const loadedProducts = await response.json();
       setProducts(loadedProducts);
+      setIsLoading(false);
     }
 
-    fetchProducts();
+    fetchProducts().catch((error) => {
+      setHttpError(error.message || 'Something went wrong.')
+      setIsLoading(false);
+    });
   }, []);
+
+  if (isLoading) {
+    return (
+      <h2 style={{ textAlign: 'center' }}>Fetching products...</h2>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <h2 style={{ textAlign: 'center' }}>{httpError}</h2>
+    );
+  }
 
   const itemClassName = `${classes.products__item} ${classes['products__item--catalog']}`;
 
