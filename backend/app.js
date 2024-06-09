6548471f54104340de4ace1d394f177ad43506ce
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-06-09 13:39:22 +0300

    Use express.Router() for requests

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
 
-import { default as db } from './data/database.js';
+import { default as shopRoutes } from './routes/shop.js';
 
 const app = express();
 
@@ -16,105 +15,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/products', async (req, res) => {
-  const query = `
-    SELECT
-    m.id,
-    m.title,
-    m.price,
-    m.large_card AS largeCard,
-    JSON_ARRAYAGG(c.name) AS availableColors
-    FROM models AS m
-    INNER JOIN models_colors AS mc ON m.id = mc.model_id
-    INNER JOIN colors AS c ON mc.color_id = c.id
-    GROUP BY m.id
-    ORDER BY m.id
-  `;
-  const [products] = await db.query(query);
-  res.json(products);
-});
-
-app.post('/orders', async (req, res) => {
-  const orderData = req.body.order;
-
-  if (orderData === null || orderData.items === null) {
-    return res
-      .status(400)
-      .json({ message: 'Missing data.' });
-  }
-
-  if (
-    orderData.customer.email === null ||
-    !orderData.customer.email.includes('@') ||
-    orderData.customer.name === null ||
-    orderData.customer.name.trim() === '' ||
-    orderData.customer.street === null ||
-    orderData.customer.street.trim() === '' ||
-    orderData.customer['postal-code'] === null ||
-    orderData.customer['postal-code'].trim() === '' ||
-    orderData.customer.city === null ||
-    orderData.customer.city.trim() === ''
-  ) {
-    return res.status(400).json({
-      message:
-        'Missing data: Email, name, street, postal code or city is missing.',
-    });
-  }
-
-  const orderId = uuidv4();
-  const newOrder = [
-    orderId,
-    orderData.total,
-    orderData.customer.name,
-    orderData.customer.email,
-    orderData.customer.street,
-    orderData.customer['postal-code'],
-    orderData.customer.city
-  ];
-
-  const orderQuery = `
-    INSERT INTO orders (
-      id,
-      total,
-      customer_name,
-      email,
-      street,
-      postal_code,
-      city
-    ) VALUES (?)
-  `;
-
-  const orderedItems = [];
-
-  for (const item of orderData.items) {
-    orderedItems.push([
-      orderId,
-      item.pId,
-      item.color,
-      item.size,
-      item.amount,
-      item.title,
-      item.price
-    ]);
-  }
-
-  const itemQuery = `
-    INSERT INTO ordered_items (
-      order_id,
-      model_id,
-      color_name,
-      size_abbr,
-      amount,
-      title,
-      price
-    ) VALUES ?
-  `;
-
-  await db.query(orderQuery, [newOrder]);
-  await db.query(itemQuery, [orderedItems]);
-
-  res.status(201).json({ message: 'Order created!' });
-});
+app.use(shopRoutes);
 
 app.use((req, res) => {
   if (req.method === 'OPTIONS') {
---
+++ backend/routes/shop.js
@@ -0,0 +1,108 @@
+import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
+
+import { default as db } from '../data/database.js';
+
+const router = express.Router();
+
+router.get('/products', async (req, res) => {
+  const query = `
+    SELECT
+    m.id,
+    m.title,
+    m.price,
+    m.large_card AS largeCard,
+    JSON_ARRAYAGG(c.name) AS availableColors
+    FROM models AS m
+    INNER JOIN models_colors AS mc ON m.id = mc.model_id
+    INNER JOIN colors AS c ON mc.color_id = c.id
+    GROUP BY m.id
+    ORDER BY m.id
+  `;
+  const [products] = await db.query(query);
+  res.json(products);
+});
+
+router.post('/orders', async (req, res) => {
+  const orderData = req.body.order;
+
+  if (orderData === null || orderData.items === null) {
+    return res
+      .status(400)
+      .json({ message: 'Missing data.' });
+  }
+
+  if (
+    orderData.customer.email === null ||
+    !orderData.customer.email.includes('@') ||
+    orderData.customer.name === null ||
+    orderData.customer.name.trim() === '' ||
+    orderData.customer.street === null ||
+    orderData.customer.street.trim() === '' ||
+    orderData.customer['postal-code'] === null ||
+    orderData.customer['postal-code'].trim() === '' ||
+    orderData.customer.city === null ||
+    orderData.customer.city.trim() === ''
+  ) {
+    return res.status(400).json({
+      message:
+        'Missing data: Email, name, street, postal code or city is missing.',
+    });
+  }
+
+  const orderId = uuidv4();
+  const newOrder = [
+    orderId,
+    orderData.total,
+    orderData.customer.name,
+    orderData.customer.email,
+    orderData.customer.street,
+    orderData.customer['postal-code'],
+    orderData.customer.city
+  ];
+
+  const orderQuery = `
+    INSERT INTO orders (
+      id,
+      total,
+      customer_name,
+      email,
+      street,
+      postal_code,
+      city
+    ) VALUES (?)
+  `;
+
+  const orderedItems = [];
+
+  for (const item of orderData.items) {
+    orderedItems.push([
+      orderId,
+      item.pId,
+      item.color,
+      item.size,
+      item.amount,
+      item.title,
+      item.price
+    ]);
+  }
+
+  const itemQuery = `
+    INSERT INTO ordered_items (
+      order_id,
+      model_id,
+      color_name,
+      size_abbr,
+      amount,
+      title,
+      price
+    ) VALUES ?
+  `;
+
+  await db.query(orderQuery, [newOrder]);
+  await db.query(itemQuery, [orderedItems]);
+
+  res.status(201).json({ message: 'Order created!' });
+});
+
+export default router;
