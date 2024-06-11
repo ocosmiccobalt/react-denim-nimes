e80b35b525eec185fbfd9449a3ae019762311d1e
Olga Bazhenova <ocosmiccobalt@gmail.com>
2024-06-11 23:58:57 +0300

    Replace Twitter icon with X icon

--- src/data/social-data.js
+++ src/data/social-data.js
@@ -2,7 +2,7 @@ const SOCIAL_DATA = [
   { id: 'tiktok', url: 'https://tiktok.com', title: 'Tiktok', w: 20, h: 20 },
   { id: 'youtube', url: 'https://youtube.com', title: 'Youtube', w: 20, h: 20 },
   { id: 'pinterest', url: 'https://www.pinterest.com', title: 'Pinterest', w: 21, h: 20 },
-  { id: 'twitter', url: 'https://twitter.com', title: 'Twitter', w: 22, h: 18 },
+  { id: 'x', url: 'https://x.com', title: 'X', w: 24, h: 24 },
 ];
 
 export default SOCIAL_DATA;
